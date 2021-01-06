14eea7b49912318a49934ee92fc039810590d4d0
brody-bishop <bkbishop@stanford.edu>
2021-01-06 12:25:34 -0800

    Normalize cross product, ECs  spawn 1 slanderer

--- src/ourplayer/EnlightenmentCenter.java
+++ src/ourplayer/EnlightenmentCenter.java
@@ -10,17 +10,21 @@ public class EnlightenmentCenter extends RobotPlayer implements RoleController {
         // RobotType toBuild = RobotType.SLANDERER;
 
         // FIRST TIME
-        if (rc.getRoundNum() == 1){
+        if (rc.getRoundNum() == 1) {
             ArrayList<MapLocation> outerRing = new ArrayList<>();
-            
+
             MapLocation loc = rc.getLocation();
 
-            for (int x = -5; x < 5; x++){
-                for (int y = -5; y < 5; y++){
-                    
+            // FOR THE LOVE OF GOD, DELETE THIS LINE AT SOME POINT
+            rc.buildRobot(RobotType.SLANDERER, Direction.NORTH, 50);
+            //////
+
+            for (int x = -5; x < 5; x++) {
+                for (int y = -5; y < 5; y++) {
+
                     // if it is in the outer beltway
 
-                    if (x*x + y*y <= 29 && x*x + y*y >=25){
+                    if (x * x + y * y <= 29 && x * x + y * y >= 25) {
                         MapLocation thisLocation = new MapLocation(x, y);
                         outerRing.add(thisLocation);
                     }
@@ -29,17 +33,13 @@ public class EnlightenmentCenter extends RobotPlayer implements RoleController {
         }
 
         int influence = rc.getInfluence();
-        
-        
-            
-        for (Direction dir : directions) {
-            if (rc.canBuildRobot(RobotType.SLANDERER, dir, influence)) {
-                rc.buildRobot(RobotType.SLANDERER, dir, influence);
-                break;
-            }
-        }
-       
-        
+
+        /*
+         * for (Direction dir : directions) { if (rc.canBuildRobot(RobotType.SLANDERER,
+         * dir, influence)) { rc.buildRobot(RobotType.SLANDERER, dir, influence); break;
+         * } }
+         */
+
         if (rc.getRoundNum() % 50 == 0) {
             System.out.println("I have " + influence + " influence on round " + rc.getRoundNum());
         }
--- src/ourplayer/Slanderer.java
+++ src/ourplayer/Slanderer.java
@@ -6,150 +6,159 @@ import java.util.Map;
 import battlecode.common.*;
 
 public class Slanderer extends RobotPlayer implements RoleController {
-    
+
     private int age;
     private MapLocation spawnEC;
     private int spawnECid;
     private double endRadius2;
 
-
-    public Slanderer(){
+    public Slanderer() {
         age = 0;
         endRadius2 = 40;
     }
 
     public void run() throws GameActionException {
-        if (age == 0){
+        if (age == 0) {
             runFirstTurn();
-        } 
+        }
 
         Direction bestMove = getBest(getPossibleMoves());
-        if(bestMove != null){
+        if (bestMove != null) {
             tryMove(bestMove);
         }
-    
+
         age++;
     }
 
-    private double calculateTargetRadius2(){
+    private double calculateTargetRadius2() {
 
-        return ((double) age * endRadius2)/300;
+        return ((double) age * endRadius2) / 300;
     }
-    private void runFirstTurn(){
-        //first turn stuff
 
-        //set spawnEC
+    private void runFirstTurn() {
+        // first turn stuff
+
+        // set spawnEC
         RobotInfo[] nearby = rc.senseNearbyRobots();
-        for (int i = 0; i < nearby.length; i++){
-            
+        for (int i = 0; i < nearby.length; i++) {
+
             // This WILL break if there is more than one EC next to
-            if (nearby[i].type == RobotType.ENLIGHTENMENT_CENTER){
+            if (nearby[i].type == RobotType.ENLIGHTENMENT_CENTER) {
                 spawnEC = nearby[i].location;
                 spawnECid = nearby[i].ID;
-                System.out.println("HOME ENLIGHTENMENT CENTER LOCATED: "+ spawnEC.x+" , "+spawnEC.y);
-                System.out.println("HOME ENLIGHTENMENT CENTER ID: " + nearby[i].ID);
+                System.out.println("HOME ENLIGHTENMENT CENTER LOCATED: " + spawnEC.x + " , " + spawnEC.y);
             }
         }
-        
+
     }
 
-    private ArrayList<Direction> getPossibleMoves(){
+    private ArrayList<Direction> getPossibleMoves() {
         ArrayList<Direction> possibleMoves = new ArrayList<>();
-        for(Direction d : Direction.values()){
-            if(rc.canMove(d)){
+        for (Direction d : Direction.values()) {
+            if (rc.canMove(d)) {
                 possibleMoves.add(d);
             }
         }
         return possibleMoves;
     }
 
-    private Direction getBest(ArrayList<Direction> possibleMoves){
-        //~~~~~~~~ MODIFY THESE TO CHANGE PRIORITIZATION ~~~~~~~~~//
-        //double kRadius = 0.5f;
-        //double kClockwise = 0.5f;
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    private Direction getBest(ArrayList<Direction> possibleMoves) {
+        // ~~~~~~~~ MODIFY THESE TO CHANGE PRIORITIZATION ~~~~~~~~~//
+        // double kRadius = 0.5f;
+        // double kClockwise = 0.5f;
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
         double bestWeight = 0.0f;
         Direction bestDirection = null;
 
-        int ecVecX = spawnEC.x - rc.getLocation().x; 
-        int ecVecY = spawnEC.x - rc.getLocation().x; 
+        int ecVecX = spawnEC.x - rc.getLocation().x;
+        int ecVecY = spawnEC.x - rc.getLocation().x;
+        double ecVecLength = Math.sqrt(ecVecX * ecVecX + ecVecY * ecVecY);
 
         int targetVecX = 0;
         int targetVecY = 0;
-        
-        for(Direction d : possibleMoves){
-            switch(d){
+        double targetVecLength = 1;
+
+        for (Direction d : possibleMoves) {
+            switch (d) {
                 case NORTH:
                     targetVecX = 0;
                     targetVecY = 1;
+                    targetVecLength = 1;
                     break;
                 case NORTHEAST:
                     targetVecX = 1;
                     targetVecY = 1;
+                    targetVecLength = 1.4142;
                     break;
                 case EAST:
                     targetVecX = 1;
                     targetVecY = 0;
+                    targetVecLength = 1;
                     break;
                 case SOUTHEAST:
                     targetVecX = 1;
                     targetVecY = -1;
+                    targetVecLength = 1.4142;
                     break;
                 case SOUTH:
                     targetVecX = 0;
                     targetVecY = -1;
+                    targetVecLength = 1;
                     break;
                 case SOUTHWEST:
                     targetVecX = -1;
                     targetVecY = -1;
+                    targetVecLength = 1.4142;
                     break;
                 case WEST:
                     targetVecX = -1;
                     targetVecY = 0;
+                    targetVecLength = 1;
                     break;
                 case NORTHWEST:
                     targetVecX = -1;
                     targetVecY = 1;
+                    targetVecLength = 1.4142;
+                    break;
+                default:
+                    // do nothing
             }
-            
-            //to calculate r^2 to given move, use the current pos (ecVec) and add the move's vector (targetVec), then r^2 it
+
+            // to calculate r^2 to given move, use the current pos (ecVec) and add the
+            // move's vector (targetVec), then r^2 it
             int moveR2 = (int) (Math.pow(ecVecX + targetVecX, 2) + Math.pow(ecVecY + targetVecY, 2));
             double deltaR2 = Math.abs(calculateTargetRadius2() - moveR2);
             // equation here for adjustment :
             // y = 1 / (ax + 1)
             // adjust a to produce steeper or smoother down
-            double r2Weight =  1/(1+deltaR2);
-            
-            double crossProduct = ecVecX * targetVecY - ecVecY * targetVecX;
+            double r2Weight = 1 / (1 + deltaR2);
 
-            double totalWeight = r2Weight*crossProduct;
+            double crossProduct = (ecVecX * targetVecY - ecVecY * targetVecX) / (ecVecLength * targetVecLength);
 
-            if(totalWeight > bestWeight){
+            double totalWeight = r2Weight * crossProduct;
+
+            System.out.println("secx:" + spawnEC.x + " myx: " + rc.getLocation().x + " ecvx: " + ecVecX);
+
+            if (totalWeight > bestWeight) {
                 bestWeight = totalWeight;
                 bestDirection = d;
-            } 
+            }
         }
         return bestDirection;
 
-         //step 1: turn directions into weighted edge graph where weight = desirablility of possible move, weight (1 ... -1)
-         /*   
-               o  o  o
-                \ | /
-              o - ø – o
-                / | \
-               o  o  o
-         */ //O_O novel coronavirus
-        
-        //step 2: weight graph including a 0 cost path to itself
-        //step 3: choose path of lowest cost
-        //will prioritize standing still over moving backwards
+        // step 1: turn directions into weighted edge graph where weight = desirablility
+        // of possible move, weight (1 ... -1)
 
-        //obstacles:
-        //  easy: prioritize radius
-        //  hard?: weight moves by how clockwise they are (negative for backwards)
-            // "perpendicularity" of vector to ec and vector to move
-            // cross product
+        // step 2: weight graph including a 0 cost path to itself
+        // step 3: choose path of lowest cost
+        // will prioritize standing still over moving backwards
+
+        // obstacles:
+        // easy: prioritize radius
+        // hard?: weight moves by how clockwise they are (negative for backwards)
+        // "perpendicularity" of vector to ec and vector to move
+        // cross product
 
     }
 
