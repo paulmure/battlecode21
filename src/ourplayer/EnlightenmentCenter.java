39d478ba330f418b3c1a069101129ae772ef6e5a
Lucas Schaberg <lschaberg@berkeley.edu>
2021-01-08 06:56:11 -0800

    add recursive pathfinder

--- src/ourplayer/EnlightenmentCenter.java
+++ src/ourplayer/EnlightenmentCenter.java
@@ -23,46 +23,45 @@ public class EnlightenmentCenter extends RobotPlayer implements RoleController {
             for (int y = -6; y <= 6; y++) {
 
                 if (x * x + y * y <= 40 && x * x + y * y >= 32) {
-                    MapLocation thisLocation = new MapLocation(rc.getLocation().x+x, rc.getLocation().y+y);
+                    MapLocation thisLocation = new MapLocation(rc.getLocation().x + x, rc.getLocation().y + y);
 
                     rc.setIndicatorDot(thisLocation, 50, 205, 50);
                     // System.out.println("drawn dot at "+thisLocation);
-                    
+
                 }
             }
         }
-        
 
         int influence = rc.getInfluence();
 
         Direction dir = randomDirection();
 
         /// BUILDING STUFF
-        // for(int i = 0; i < 8; i++){
-        //     if (rc.getRoundNum() < 300 && rc.getRoundNum() % 5 == 0){
-        //         if(tryBuildRobot(RobotType.POLITICIAN, dir, 50)){
-        //             break;
-        //         }
-        //     } else {
-        //         if(tryBuildRobot(RobotType.SLANDERER, dir, 50)){
-        //             activeSlanderers.add(50 / 20);
-        //             if(activeSlanderers.size() > 50) {
-        //                 activeSlanderers.poll();
-        //             }
-        //             break;
-        //         }
-        //     } 
-        //     dir = dir.rotateRight();
-        // }
-
-        // int influencePerTurn = getECIncome();
-        // for(Integer i : activeSlanderers) {
-        //     influencePerTurn += i;
-        // }
-        /////////////////////////
-        if (rc.getRoundNum() == 1){
-            tryBuildRobot(RobotType.MUCKRAKER, directions[1], 50);
+        for (int i = 0; i < 8; i++) {
+            if (rc.getRoundNum() < 300 && rc.getRoundNum() % 5 == 0) {
+                if (tryBuildRobot(RobotType.POLITICIAN, dir, 50)) {
+                    break;
+                }
+            } else {
+                if (tryBuildRobot(RobotType.SLANDERER, dir, 50)) {
+                    activeSlanderers.add(50 / 20);
+                    if (activeSlanderers.size() > 50) {
+                        activeSlanderers.poll();
+                    }
+                    break;
+                }
+            }
+            dir = dir.rotateRight();
         }
+
+        int influencePerTurn = getECIncome();
+        for (Integer i : activeSlanderers) {
+            influencePerTurn += i;
+        }
+        /////////////////////////
+        // if (rc.getRoundNum() == 1) {
+        // tryBuildRobot(RobotType.MUCKRAKER, Direction.NORTH, 50);
+        // }
 
         if (rc.getRoundNum() % 50 == 0) {
             System.out.println("I have " + influence + " influence on round " + rc.getRoundNum());
