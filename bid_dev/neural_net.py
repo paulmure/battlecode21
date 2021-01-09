d00f5f7e1a74c0ec8503532f21290d0cbee4dd44
paulmure <paulmure2000@gmail.com>
2021-01-09 15:04:06 -0800

    update bidding information

--- bid_dev/neural_net.py
+++ bid_dev/neural_net.py
@@ -5,5 +5,8 @@ class BidState(StructClass):
     round_number: int      # the current round number, starts at 1
     influence: int         # how much influence we currently have
     last_price: int        # the price we bid on last round
-    last_outcome: str      # the result of the last bid: { 'won', 'lost' }
-    streak: int            # how many times in a row did the last result happen
+    last_outcome: int      # the result of the last bid: won = +1, lose = -1
+    streak: int            # how many of the last outcome in a row, win = + , lose = -
+    income_per_turn: int
+    votes_won: int
+    votes_lost: int
